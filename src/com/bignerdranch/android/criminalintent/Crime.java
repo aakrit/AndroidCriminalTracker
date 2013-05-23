7d7db62d39f64192921b58a53c4042e85bf80dc7
Adam Gerber <adam.gerber@yahoo.com>
2013-05-23 16:52:46 -0500

    ch08: layouts and widgets

--- src/com/bignerdranch/android/criminalintent/Crime.java
+++ src/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,14 +1,17 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
-    
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
     
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public String getTitle() {
@@ -22,4 +25,22 @@ public class Crime {
     public UUID getId() {
         return mId;
     }
+
+	public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+    
+    public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+    
 }
